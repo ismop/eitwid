bb5050c2013978ed62c805a9a1678a0eea1823bd
Daniel Harezlak <d.harezlak@cyfronet.pl>
2014-02-07 13:44:19 +0100

    simple web test added

--- src/test/java/pl/ismop/web/controllers/MainControllerTest.java
+++ src/test/java/pl/ismop/web/controllers/MainControllerTest.java
@@ -1,5 +1,39 @@
 package pl.ismop.web.controllers;
 
-public class MainControllerTest {
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import pl.ismop.web.Application;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class MainControllerTest {
+	@Autowired private WebApplicationContext wac;
+	private MockMvc mockMvc;
+	
+	@Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+	
+	@Test
+    public void shouldRedirectToLogin() throws Exception {
+        this.mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("Main page")));
+    }
 }
