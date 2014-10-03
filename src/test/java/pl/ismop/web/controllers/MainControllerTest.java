740d43286d83df82c382bbadc52db355308d810b
Daniel Harezlak <d.harezlak@cyfronet.pl>
2014-10-03 08:54:28 +0200

    missing csrf token in tests bug fixed

--- src/test/java/pl/ismop/web/controllers/MainControllerTest.java
+++ src/test/java/pl/ismop/web/controllers/MainControllerTest.java
@@ -10,6 +10,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,15 +27,23 @@ import pl.ismop.web.Application;
 @WebAppConfiguration
 public class MainControllerTest {
 	@Autowired private WebApplicationContext wac;
+	@Autowired private MockHttpSession session;
+	
 	private MockMvc mockMvc;
 	
 	@Before
     public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+        this.mockMvc = MockMvcBuilders
+        		.webAppContextSetup(this.wac)
+        		.build();
     }
 	
 	@Test
     public void shouldRedirectToLogin() throws Exception {
+		HttpSessionCsrfTokenRepository httpSessionCsrfTokenRepository = new HttpSessionCsrfTokenRepository();
+		CsrfToken csrfToken = httpSessionCsrfTokenRepository.generateToken(new MockHttpServletRequest());
+		session.setAttribute("_csrf", csrfToken);
+		
         this.mockMvc.perform(get("/"))
             .andExpect(status().isOk())
             .andExpect(content().string(containsString("ISMOP - Summary")));
