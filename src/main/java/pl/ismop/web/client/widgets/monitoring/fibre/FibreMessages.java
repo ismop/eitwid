540cafddf0ca5a9512505d87afc9cee5429f92a4
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-09-24 11:43:51 +0200

    Fibre view modal title externalized

--- src/main/java/pl/ismop/web/client/widgets/monitoring/fibre/FibreMessages.java
+++ src/main/java/pl/ismop/web/client/widgets/monitoring/fibre/FibreMessages.java
@@ -3,6 +3,7 @@ package pl.ismop.web.client.widgets.monitoring.fibre;
 import com.google.gwt.i18n.client.Messages;
 
 public interface FibreMessages extends Messages {
+    String title();
     String fibreChartTitle();
     String firbreChartXAxisTitle();
     String deviceChartInitTitle();
@@ -10,8 +11,6 @@ public interface FibreMessages extends Messages {
     String loadingDevicesValues();
     String loadingFibreShare();
     String loadingData();
-
     String deviceTooltip(String value, String leveeDistanceMarker, String cableDistanceMarker, String sensorId);
-
     String errorLoadingDataFromDap();
 }
