b5c37c892cd1a2007789f122916e3321f9dbfd9b
Daniel Harezlak <d.harezlak@cyfronet.pl>
2015-09-25 09:06:56 +0200

    closes #12 view switching and stub analysis widgets in place

---
+++ src/main/java/pl/ismop/web/client/widgets/analysis/comparison/IComparisonView.java
@@ -0,0 +1,9 @@
+package pl.ismop.web.client.widgets.analysis.comparison;
+
+import com.google.gwt.user.client.ui.IsWidget;
+
+public interface IComparisonView extends IsWidget {
+	interface IComparisonPresenter {
+		
+	}
+}
---
+++ src/main/java/pl/ismop/web/client/widgets/analysis/sidepanel/IAnalysisSidePanel.java
@@ -0,0 +1,9 @@
+package pl.ismop.web.client.widgets.analysis.sidepanel;
+
+import com.google.gwt.user.client.ui.IsWidget;
+
+public interface IAnalysisSidePanel extends IsWidget {
+	interface IAnalysisSidePanelPresenter {
+		
+	}
+}
