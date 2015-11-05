d1e9a35a310705ce967b42a61f6bc6ca84d66936
Bartosz Wilk <b.wilk@cyfronet.pl>
2015-11-05 19:30:47 +0100

    Parameters sorted by name. Removed unused class GroupedReadings

--- src/main/java/pl/ismop/web/client/widgets/monitoring/weather/WeatherReadings.java
+++ src/main/java/pl/ismop/web/client/widgets/monitoring/weather/WeatherReadings.java
@@ -96,13 +96,10 @@ public class WeatherReadings {
 	}
 	
 	private Comparator<Parameter> parameterComparator = new Comparator<Parameter>() {
-
 		@Override
 		public int compare(Parameter o1, Parameter o2) {
-			return o1.getId().compareTo(o2.getId());
+			return o1.getParameterName().compareTo(o2.getParameterName());
 		}
 	};
-
-
 		
 }
