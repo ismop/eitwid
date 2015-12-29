622b07b29e9a626c3460b5c159a165cce48af00b
Daniel Harezlak <d.harezlak@cyfronet.pl>
2015-12-29 11:47:07 +0100

    closes #57 export configuration set globally for all highcharts charts

--- src/main/java/pl/ismop/web/client/IsmopWebEntryPoint.java
+++ src/main/java/pl/ismop/web/client/IsmopWebEntryPoint.java
@@ -1,10 +1,13 @@
 package pl.ismop.web.client;
 
+import java.util.Date;
+
 import org.fusesource.restygwt.client.Defaults;
 import org.fusesource.restygwt.client.ServiceRoots;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.i18n.client.Dictionary;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -20,8 +23,59 @@ public class IsmopWebEntryPoint implements EntryPoint {
 		ServiceRoots.add("hypgen", properties.get("hypgenEndpoint"));
 		Defaults.ignoreJsonNulls();
 		
+		GlobalMessages globalMessages = GWT.create(GlobalMessages.class);
+		globalInitialization(globalMessages);
+		
 		Mvp4gModule module = (Mvp4gModule)GWT.create(Mvp4gModule.class);
 		module.createAndStartModule();
 		RootLayoutPanel.get().add((Widget) module.getStartView());
 	}
+
+	private void globalInitialization(GlobalMessages globalMessages) {
+		//this is the place to do global initialization once
+		configureHighchartsGlobalSettings(globalMessages.exportPngLabel(), globalMessages.exportPdfLabel(), globalMessages.exportSvgLabel());
+	}
+	
+	private String getCurrentDate() {
+		DateTimeFormat format = DateTimeFormat.getFormat("yyyy-MM-dd_HH:mm:ss");
+		
+		return format.format(new Date());
+	}
+
+	private native void configureHighchartsGlobalSettings(String pngLabel, String pdfLabel, String svgLabel) /*-{
+		var object = this;
+		$wnd.Highcharts.setOptions({
+			exporting: {
+				buttons: {
+					contextButton: {
+						menuItems: [{
+							text: pngLabel,
+							onclick: function() {
+								this.exportChart({
+									filename: "ismop-data-export-" + object.@pl.ismop.web.client.IsmopWebEntryPoint::getCurrentDate()(),
+									type: "image/png"
+								})
+							}
+						}, {
+							text: pdfLabel,
+							onclick: function() {
+								this.exportChart({
+									filename: "ismop-data-export-" + object.@pl.ismop.web.client.IsmopWebEntryPoint::getCurrentDate()(),
+									type: "application/pdf"
+								})
+							}
+						}, {
+							text: svgLabel,
+							onclick: function() {
+								this.exportChart({
+									filename: "ismop-data-export-" + object.@pl.ismop.web.client.IsmopWebEntryPoint::getCurrentDate()(),
+									type: "image/svg+xml"
+								})
+							}
+						}]
+					}
+				}
+			}
+		});
+	}-*/;
 }
