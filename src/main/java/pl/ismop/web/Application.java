44484f44e196b73bd1937999280955adf5c6c4a8
Daniel Harezlak <d.harezlak@cyfronet.pl>
2016-08-30 11:28:09 +0200

    closes #129: GWT upgraded, unused dependencies removed

--- src/main/java/pl/ismop/web/Application.java
+++ src/main/java/pl/ismop/web/Application.java
@@ -1,27 +1,8 @@
 package pl.ismop.web;
 
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.ClientRequestContext;
-import javax.ws.rs.client.ClientRequestFilter;
-import javax.ws.rs.client.WebTarget;
-
 import org.apache.catalina.Context;
 import org.apache.catalina.session.FileStore;
 import org.apache.catalina.session.PersistentManager;
-import org.glassfish.jersey.client.proxy.WebResourceFactory;
-import org.glassfish.jersey.jackson.JacksonFeature;
-import org.mitre.dsmiley.httpproxy.ProxyServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +11,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
@@ -39,37 +19,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import pl.ismop.web.client.dap.levee.LeveeServiceSync;
-import pl.ismop.web.client.dap.section.SectionServiceSync;
-import pl.ismop.web.client.dap.sensor.SensorServiceSync;
-import pl.ismop.web.misc.HttpsIgnoringProxyServlet;
-import pl.ismop.web.services.DapService;
-
 @EnableAutoConfiguration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 @ComponentScan
 public class Application extends WebMvcConfigurerAdapter {
+
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
-	
+
 	@Value("${dap.token}") private String dapToken;
 	@Value("${dap.endpoint}") private String dapEndpoint;
 	@Value("${public.maps.server.url}") private String mapsServerUrl;
 	@Value("${session.store.path}") private String sessionStorePath;
 	@Value("${hypgen.endpoint}") private String hypgenEndpoint;
-	
-	class AuthFilter implements ClientRequestFilter {
-		@Override
-		public void filter(ClientRequestContext requestContext) throws IOException {
-			requestContext.getHeaders().add("PRIVATE-TOKEN", dapToken);
-		}
-	}
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).run(args);
@@ -106,98 +74,7 @@ public class Application extends WebMvcConfigurerAdapter {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	@Bean
-	public DapService dapService(LeveeServiceSync leveeService, SensorServiceSync sensorService, SectionServiceSync profileService) {
-		return new DapService(leveeService, sensorService, profileService);
-	}
-	
-	@Bean
-	public LeveeServiceSync leveeServiceSync(WebTarget target) throws KeyManagementException, NoSuchAlgorithmException {
-		LeveeServiceSync leveeService = WebResourceFactory.newResource(LeveeServiceSync.class, target);
-		
-		return leveeService;
-	}
-	
-	@Bean
-	public SensorServiceSync sensorServiceSync(WebTarget target) throws KeyManagementException, NoSuchAlgorithmException {
-		SensorServiceSync serviceService = WebResourceFactory.newResource(SensorServiceSync.class, target);
-		
-		return serviceService;
-	}
-	
-	@Bean
-	public SectionServiceSync profileServiceSync(WebTarget target) throws KeyManagementException, NoSuchAlgorithmException {
-		SectionServiceSync profileService = WebResourceFactory.newResource(SectionServiceSync.class, target);
-		
-		return profileService;
-	}
 
-	@Bean
-	public WebTarget webTarget() throws NoSuchAlgorithmException,
-			KeyManagementException {
-		SSLContext sslContext = SSLContext.getInstance("SSL");
-		sslContext.init(null, new TrustManager[] { new X509TrustManager() {
-			public X509Certificate[] getAcceptedIssuers() {
-				return null;
-			}
-
-			public void checkClientTrusted(X509Certificate[] certs, String authType) {
-			}
-
-			public void checkServerTrusted(X509Certificate[] certs, String authType) {
-			}
-		} }, new SecureRandom());
-		Client client = ClientBuilder.newBuilder()
-				.sslContext(sslContext)
-				.hostnameVerifier((String hostname, SSLSession session) -> {
-					return true;
-				})
-				.build();
-		client.register(new AuthFilter());
-		client.register(JacksonFeature.class);
-		
-		WebTarget target = client.target(dapEndpoint);
-		
-		return target;
-	}
-	
-	@Bean
-	public ServletRegistrationBean maps() {
-		ServletRegistrationBean registration = new ServletRegistrationBean();
-		registration.setServlet(new ProxyServlet());
-		registration.addInitParameter("targetUri", mapsServerUrl);
-		registration.addInitParameter("log", "true");
-		registration.setName("wmsproxy");
-		registration.addUrlMappings("/wmsproxy/*");
-		
-		return registration;
-	}
-	
-	@Bean
-	public ServletRegistrationBean dapProxy() {
-		ServletRegistrationBean registration = new ServletRegistrationBean();
-		registration.setServlet(new HttpsIgnoringProxyServlet());
-		registration.addInitParameter("targetUri", dapEndpoint);
-		registration.addInitParameter("log", "true");
-		registration.setName("dapproxy");
-		registration.addUrlMappings("/dapproxy/*");
-		
-		return registration;
-	}
-	
-	@Bean
-	public ServletRegistrationBean hypgenProxy() {
-		ServletRegistrationBean registration = new ServletRegistrationBean();
-		registration.setServlet(new HttpsIgnoringProxyServlet());
-		registration.addInitParameter("targetUri", hypgenEndpoint);
-		registration.addInitParameter("log", "true");
-		registration.setName("hypgenproxy");
-		registration.addUrlMappings("/hypgenproxy/*");
-		
-		return registration;
-	}
-	
 	@Bean
 	public EmbeddedServletContainerFactory servletContainer() {
 		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
@@ -206,13 +83,13 @@ public class Application extends WebMvcConfigurerAdapter {
 			public void customize(Context context) {
 				FileStore store = new FileStore();
 				store.setDirectory(sessionStorePath);
-				
+
 				PersistentManager manager = new PersistentManager();
 				manager.setStore(store);
 				context.setManager(manager);
 			}
 		});
-		
+
 	    return factory;
 	}
 }
--- src/main/java/pl/ismop/web/client/widgets/common/profile/SideProfileScene.java
+++ src/main/java/pl/ismop/web/client/widgets/common/profile/SideProfileScene.java
@@ -10,18 +10,15 @@ import thothbot.parallax.core.client.AnimatedScene;
 import thothbot.parallax.core.client.RenderingPanel;
 import thothbot.parallax.core.shared.cameras.PerspectiveCamera;
 import thothbot.parallax.core.shared.core.ExtrudeGeometry;
-import thothbot.parallax.core.shared.core.Geometry;
 import thothbot.parallax.core.shared.core.Raycaster;
 import thothbot.parallax.core.shared.core.Raycaster.Intersect;
 import thothbot.parallax.core.shared.curves.Shape;
 import thothbot.parallax.core.shared.geometries.CylinderGeometry;
 import thothbot.parallax.core.shared.geometries.PlaneGeometry;
 import thothbot.parallax.core.shared.lights.PointLight;
-import thothbot.parallax.core.shared.materials.LineBasicMaterial;
 import thothbot.parallax.core.shared.materials.MeshLambertMaterial;
 import thothbot.parallax.core.shared.math.Color;
 import thothbot.parallax.core.shared.math.Vector3;
-import thothbot.parallax.core.shared.objects.Line;
 import thothbot.parallax.core.shared.objects.Mesh;
 import thothbot.parallax.core.shared.scenes.FogExp2;
 
@@ -33,7 +30,7 @@ public class SideProfileScene extends AnimatedScene {
 	private RenderingPanel renderingPanel;
 	private Raycaster raycaster;
 	private List<Mesh> sensors;
-	
+
 	public SideProfileScene() {
 		sensors = new ArrayList<>();
 	}
@@ -41,10 +38,10 @@ public class SideProfileScene extends AnimatedScene {
 	@Override
 	protected void onUpdate(double duration) {
 		Vector3 mouse = new Vector3(mouseX, mouseY, 1);
-		
+
 		raycaster.set(camera.getPosition(), mouse.sub(camera.getPosition()).normalize());
 		List<Raycaster.Intersect> intersects = raycaster.intersectObjects(sensors, false);
-		
+
 		if(intersects.size() > 0) {
 			for(Intersect intersected : intersects) {
 				intersected.object.getMaterial().setOpacity(0.5);
@@ -52,7 +49,7 @@ public class SideProfileScene extends AnimatedScene {
 		} else {
 			sensors.get(0).getMaterial().setOpacity(1.0);
 		}
-		
+
 		getRenderer().render(getScene(), camera);
 	}
 
@@ -62,68 +59,67 @@ public class SideProfileScene extends AnimatedScene {
 		camera = new PerspectiveCamera(70, getRenderer().getAbsoluteAspectRation(), 1, 500);
 		camera.setPosition(new Vector3(-70, 70, 100));
 		camera.lookAt(new Vector3(-20, 0, 0));
-		
-//		addAxes();
+
 		FogExp2 fog = new FogExp2(0xbebebe, 0.003);
 		getScene().setFog(fog);
-		
+
 		PointLight light = new PointLight(0xffffff);
 		light.setPosition(new Vector3(0, 100, 100));;
 		light.setIntensity(1.2);
 		getScene().add(light);
-		
+
 		Shape profileSide = new Shape();
 		profileSide.moveTo(0, 0);
 		profileSide.lineTo(200, 0);
 		profileSide.lineTo(125, 50);
 		profileSide.lineTo(75, 50);
 		profileSide.lineTo(0, 0);
-		
+
 		ExtrudeGeometry.ExtrudeGeometryParameters extrudeSettings = new ExtrudeGeometry.ExtrudeGeometryParameters();
 		extrudeSettings.amount = 300;
 		extrudeSettings.steps = 2;
 		extrudeSettings.bevelEnabled = false;
-		
+
 		MeshLambertMaterial profileMaterial = new MeshLambertMaterial();
 		profileMaterial.setColor(new Color(0xe1b154));
-		
+
 		ExtrudeGeometry profile = profileSide.extrude(extrudeSettings);
 		mesh = new Mesh(profile, profileMaterial);
 		mesh.setPosition(new Vector3(-100, 0, -300));
 		getScene().add(mesh);
-		
+
 		MeshLambertMaterial planeMaterial = new MeshLambertMaterial();
 		planeMaterial.setColor(new Color(0xa2e56d));
-		
+
 		Mesh plane = new Mesh(new PlaneGeometry(1000, 1000, 100, 100), planeMaterial);
 		plane.getRotation().addX(270 * Math.PI / 180);
 		getScene().add(plane);
-		
+
 		Shape waterShape = new Shape();
 		waterShape.moveTo(200, 0);
 		waterShape.lineTo(400, 0);
 		waterShape.lineTo(400, 30);
 		waterShape.lineTo(150, 30);
 		waterShape.lineTo(200, 0);
-		
+
 		ExtrudeGeometry water = waterShape.extrude(extrudeSettings);
 		MeshLambertMaterial waterMaterial = new MeshLambertMaterial();
 		waterMaterial.setColor(new Color(0x3d90dd));
 		waterMaterial.setOpacity(0.6);
-		
+
 		Mesh waterMesh = new Mesh(water, waterMaterial);
 		waterMesh.setPosition(new Vector3(-100, 0, -301));
 		getScene().add(waterMesh);
-		
+
 		MeshLambertMaterial sensorMaterial = new MeshLambertMaterial();
 		sensorMaterial.setColor(new Color(0xf44f4f));
-		
+
 		CylinderGeometry sensor = new CylinderGeometry(2, 2, 20, 10, 10);
 		Mesh sensorMesh = new Mesh(sensor, sensorMaterial);
 		sensorMesh.setPosition(new Vector3(-70, 20, 0));
 		sensors.add(sensorMesh);
 		getScene().add(sensorMesh);
-		
+
 		renderingPanel.getCanvas().addMouseMoveHandler(new MouseMoveHandler() {
 			@Override
 			public void onMouseMove(MouseMoveEvent event) {
@@ -133,26 +129,6 @@ public class SideProfileScene extends AnimatedScene {
 				mouseY = event.getY();
 			}
 		});
-	}
-
-	private void addAxes() {
-		float length = 1000;
-		getScene().add(createAxis(new Vector3(-length, 0, 0), new Vector3(length, 0, 0), 0xff0000));
-		getScene().add(createAxis(new Vector3(0, -length, 0), new Vector3(0, length, 0), 0x00ff00));
-		getScene().add(createAxis(new Vector3(0, 0, -length), new Vector3(0, 0, length), 0x0000ff));
-	}
-
-	private Line createAxis(Vector3 from, Vector3 to, int color) {
-		Geometry geometry = new Geometry();
-		geometry.getVertices().add(from);
-		geometry.getVertices().add(to);
-		
-		Line result = new Line(geometry);
-		LineBasicMaterial material = new LineBasicMaterial();
-		material.setColor(new Color(color));
-		result.setMaterial(material);
-		
-		return result;
 	}
 
 	public void setRenderingPanel(RenderingPanel renderingPanel) {
