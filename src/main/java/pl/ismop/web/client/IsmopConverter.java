4af09aa46cc3aee5079b5c29d5812eb8960f680e
Daniel Harezlak <d.harezlak@cyfronet.pl>
2016-11-03 14:02:34 +0100

    #158: localized date literals overwritten

--- src/main/java/pl/ismop/web/client/IsmopConverter.java
+++ src/main/java/pl/ismop/web/client/IsmopConverter.java
@@ -13,52 +13,76 @@ import com.google.inject.Singleton;
 public class IsmopConverter {
     private final DateTimeFormat dtoDateTimeFormat =
             DateTimeFormat.getFormat(DateTimeFormat.PredefinedFormat.ISO_8601);
-    
+
     private final DateTimeFormat displayDateTimeFormat =
             DateTimeFormat.getFormat(DateTimeFormat.PredefinedFormat.DATE_TIME_MEDIUM);
-    
+
     private final DateTimeFormat fileSafeNameDateTimeFormat =
             DateTimeFormat.getFormat("yyyy-MM-dd_HH:mm:ss");
 
     public String formatForDisplay(Date date) {
         return displayDateTimeFormat.format(date);
     }
-    
+
     public String formatForDto(Date date) {
         return dtoDateTimeFormat.format(date);
     }
-    
+
     public List<String> shortMonths() {
     	List<String> result = new ArrayList<>();
     	DateTimeFormat shortMonths = DateTimeFormat.getFormat("MMM");
-    	
+
     	for (int i = 0; i < 12; i++) {
     		result.add(shortMonths.format(new Date(1900, i, 1)));
     	}
-    	
+
     	return result;
     }
-    
+
     public List<String> months() {
     	List<String> result = new ArrayList<>();
     	DateTimeFormat shortMonths = DateTimeFormat.getFormat("MMMM");
-    	
+
     	for (int i = 0; i < 12; i++) {
     		result.add(shortMonths.format(new Date(1900, i, 1)));
     	}
-    	
+
     	return result;
     }
 
 	public List<String> days() {
 		List<String> result = new ArrayList<>();
     	DateTimeFormat shortMonths = DateTimeFormat.getFormat("EEEE");
-    	
+
     	for (int i = 1; i <= 7; i++) {
     		//4 added to start with Sunday
     		result.add(shortMonths.format(new Date(1900, 0, i + 4)));
     	}
-    	
+
+    	return result;
+	}
+
+	public List<String> shortDays() {
+		List<String> result = new ArrayList<>();
+    	DateTimeFormat shortMonths = DateTimeFormat.getFormat("EEE");
+
+    	for (int i = 1; i <= 7; i++) {
+    		//4 added to start with Sunday
+    		result.add(shortMonths.format(new Date(1900, 0, i + 4)));
+    	}
+
+    	return result;
+	}
+
+	public List<String> minDays() {
+		List<String> result = new ArrayList<>();
+    	DateTimeFormat shortMonths = DateTimeFormat.getFormat("E");
+
+    	for (int i = 1; i <= 7; i++) {
+    		//4 added to start with Sunday
+    		result.add(shortMonths.format(new Date(1900, 0, i + 4)));
+    	}
+
     	return result;
 	}
 
@@ -100,11 +124,11 @@ public class IsmopConverter {
 
 		return builder.toString();
 	}
-	
+
 	public String getCurrentLocale() {
 		return LocaleInfo.getCurrentLocale().getLocaleName();
 	}
-	
+
 	public String getSectionFillColor(String colourType) {
 		switch (colourType) {
 			case "A":
---
+++ src/main/resources/org/gwtbootstrap3/extras/datetimepicker/client/resource/js/locales-2.3.8/bootstrap-datetimepicker.pl.js
@@ -0,0 +1,17 @@
+/**
+ * Polish translation for bootstrap-datetimepicker
+ * Robert <rtpm@gazeta.pl>
+ */
+;(function($){
+$.fn.datetimepicker.dates['pl'] = {
+        days: ["niedziela", "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela"],
+        daysShort: ["nie", "pon", "wto", "śro", "czw", "pią", "sob", "nie"],
+        daysMin: ["ni", "po", "wt", "śr", "cz", "pi", "so", "ni"],
+        months: ["styczneń", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"],
+        monthsShort: ["sty", "lut", "mar", "kwi", "maj", "cze", "lip", "sie", "wrz", "paź", "lis", "gru"],
+        today: "Dzisiaj",
+		suffix: [],
+		meridiem: [],
+        weekStart: 1
+};
+}(jQuery));
